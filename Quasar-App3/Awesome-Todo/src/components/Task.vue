ad3011135101d2367f3c57862223cb09ebd94ae2
Nguyen Thanh Minh <thanhminh339968az@gmail.com>
2022-10-15 12:37:23 +0700

    Lesson-03

---
+++ Quasar-App3/Awesome-Todo/src/components/Task.vue
@@ -0,0 +1,11 @@
+<teamplate>
+    <li>
+        <div>{{ task.name }} {{ index }}</div>
+        <small>{{ task.duaDate }} @ {{ task.dueTime }}</small>
+        <button @click="deleteTask(index)">X</button>
+    </li>
+</teamplate>
+
+<script>
+
+</script>
---
+++ Quasar-App3/Awesome-Todo/src/pages/Index.vue
@@ -0,0 +1,49 @@
+<template>
+  <q-page padding>
+    <ul>
+      <li v-for="(task, index) in tasks">
+        <div>{{ task.name }} {{ index }}</div>
+        <small>{{ task.duaDate }} @ {{ task.dueTime }}</small>
+        <button @click="deleteTask(index)">X</button>
+      </li>
+    </ul>
+  </q-page>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        tasks: [
+          {
+            name: 'Go to shop',
+            dueDate: '2019/05/12',
+            dueTime: '18:30'
+          },
+          {
+            name: 'Get bananas',
+            dueDate: '2019/05/13',
+            dueTime: '14:30'
+          },
+          {
+            name: 'Get apples',
+            dueDate: '2019/05/14',
+            dueTime: '16:30'
+          },
+        ]
+      }
+    },
+    methods: {
+      deleteTask(index){
+        this.tasks.splice(index,1)
+      }
+    },
+    components: {
+      'task' : require('components/Task.vue').default
+    }
+  }
+</script>
+
+<style>
+
+</style>
