415b0c98e0c75a2624b33068f4c1d3096d21b7d2
Nguyen Thanh Minh <thanhminh339968az@gmail.com>
2022-10-17 17:47:33 +0700

    Lesson-06

---
+++ Quasar-App6/Awesome-Todo/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="q-app">
+    <router-view />
+  </div>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "App",
+});
+</script>
+
+<style>
+ .text-strikethrough {
+  text-decoration: line-through;
+ }
+</style>
