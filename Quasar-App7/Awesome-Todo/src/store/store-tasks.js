5022bdcb1cf044fa4cb869076a1ec4328605ee2d
Nguyen Thanh Minh <thanhminh339968az@gmail.com>
2022-10-18 16:51:01 +0700

    Lesson-07

---
+++ Quasar-App7/Awesome-Todo/src/store/store-tasks.js
@@ -0,0 +1,44 @@
+const state = {
+    tasks: {
+        'ID1': {
+          name: 'Go to shop',
+          completed: false,
+          dueDate:'2019/05/12',
+          dueTime: '18:30'
+        },
+        'ID2': {
+          name: 'Get bananas',
+          completed: false,
+          dueDate:'2019/05/13',
+          dueTime: '14:00'
+        },
+        'ID3': {
+          name: 'Get apples',
+          completed: false,
+          dueDate:'2019/05/14',
+          dueTime: '16:00'
+        }
+      }
+    }
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+const getters = {
+    tasks: (state) => {
+        return state.tasks
+    }
+}
+
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions,
+    getters
+}
